af9bfcf4c75737436a9fc9e917777be6b9022a62
vthong272 <thongvinh2@gmail.com>
2025-04-25 20:32:23 +0700

    add particleJs and reponsive web for full screen

---
+++ app.js
@@ -0,0 +1,26 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 80,
+        density: { enable: true, value_area: 800 },
+      },
+      color: { value: "#fff" },
+      shape: { type: "circle" },
+      opacity: { value: 1, random: true },
+      size: { value: 3, random: true },
+      line_linked: {
+        enable: true,
+        distance: 350,
+        color: "#9932CC",
+        opacity: 0.2,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        random: true,
+        out_mode: "out",
+      },
+    },
+  });
